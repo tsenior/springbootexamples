4a955c867def32d4a3febabe538553a5691e4c13
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-07-19 01:32:27 +0200

    first commit

---
+++ src/main/java/com/github/tsenior/springexamples/SpringexamplesApplication.java
@@ -0,0 +1,13 @@
+package com.github.tsenior.springexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringexamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringexamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/tsenior/springexamples/SpringexamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tsenior.springexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringexamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
